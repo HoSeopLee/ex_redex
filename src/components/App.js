2662890cf13309c70b131382a9c9b9a0aa65b304
LeeHoSeop <llss2ssll@gmail.com>
2019-08-14 15:17:59 +0900

    test

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Counter from './Counter';
+import Buttons from './Buttons';
+import Option from './Option';
+function App() {
+  return (
+    <div style={{textAlign:'center'}}>
+      <Counter/>
+      <Option/>
+      <Buttons/>
+    </div>
+  );
+}
+
+export default App;
